8183c312d0812eef55e795b4ed304406c5c12f07
John Nguyen <ipwnponies@gmail.com>
2020-12-23 00:43:50 -0800

    Tally categories and tags and print tabulated output

--- tools/parse_categories.py
+++ tools/parse_categories.py
@@ -1,6 +1,8 @@
+from collections import defaultdict, Counter
 from pathlib import Path
 
 import frontmatter
+import tabulate
 import typer
 
 
@@ -11,27 +13,34 @@ def main(
     if not (show_categories or show_tags):
         raise typer.Exit('Did not choose to print categories or tags')
 
-    categories = set()
-    tags = set()
+    categories = defaultdict(int)
+    tags = defaultdict(int)
 
     for file in Path('.').glob('_posts/*.md'):
         metadata, _ = frontmatter.parse(file.open().read())
-        post_categories = metadata.get('categories')
+        post_categories = metadata.get('categories') or []
 
-        if isinstance(post_categories, list):
-            categories.update(set(post_categories))
-        elif isinstance(post_categories, str):
-            categories.add(post_categories)
+        if isinstance(post_categories, str):
+            post_categories = [post_categories]
 
-        post_tags = metadata.get('tags')
-        if post_tags:
-            tags.update(set(post_tags))
+        for i in post_categories:
+            categories[i] += 1
+
+        post_tags = metadata.get('tags') or []
+        for i in post_tags:
+            tags[i] += 1
 
     if show_categories:
-        print('\n'.join(categories))
+        print(tabulate.tabulate(
+            Counter(categories).most_common(),
+            headers=['category', 'count']
+        ))
 
     if show_tags:
-        print('\n'.join(tags))
+        print(tabulate.tabulate(
+            Counter(tags).most_common(),
+            headers=['tag', 'count']
+        ))
 
 
 if __name__ == '__main__':
